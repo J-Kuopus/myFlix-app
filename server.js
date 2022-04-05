0f51b4d2d5aef3473499fd69cb56681aaea8e734
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-05 13:28:50 +0200

    Corrected syntax

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 const http = require('http'),
-fs = require('fs'),
-url = require('url');
+  fs = require('fs'),
+  url = require('url');
 
 http
   .createServer((request, reponse) => {
     let addr = request.url,
-    q = url.parse(addr, true),
-    filePath _ '';
+      q = url.parse(addr, true),
+      filePath = '';
 
     if (q.pathname.includes('documenation')) {
-      filePath = (__dirname + '/documentation.html');
+      filePath = __dirname + '/documentation.html';
     } else {
       filePath = 'index.html';
     }
 
-    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',
-    (err) => {
-      if (err) {
-        console.log(err);
+    fs.appendFile(
+      'log.txt',
+      'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',
+      (err) => {
+        if (err) {
+          console.log(err);
         } else {
           consonle.log('Added to log.');
         }
-    });
+      }
+    );
 
     fs.readFile(filePath, (err, data) => {
       if (err) {
         throw err;
       }
-  
-    Response.writeHead(200, { 'Content-Type': 'text/html' });
-    Response.write(data);
-    Response.end();
-  });
-})
+
+      Response.writeHead(200, { 'Content-Type': 'text/html' });
+      Response.write(data);
+      Response.end();
+    });
+  })
   .listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
