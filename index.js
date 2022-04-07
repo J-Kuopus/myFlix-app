8ebf3bf3f751483093234ecba8b4065cd761f0e8
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-07 10:24:27 +0200

    Moved use method down the list
    It wasn't working when placed before the two GET methods

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ let topMovies = [
   { title: 'Paprika', director: 'Satoshi Kon' },
 ];
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
@@ -23,6 +21,8 @@ app.get('/', (req, res) => {
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
+
+app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
