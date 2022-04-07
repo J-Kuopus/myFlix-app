b33b334aa135c1a75f0b016fb5b1e42a1507c305
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-07 09:49:08 +0200

    Added listen request

--- index.js
+++ index.js
@@ -21,3 +21,7 @@ app.get('/movies', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
