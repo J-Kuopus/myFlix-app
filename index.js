c34df2db9fe13639ffd1cf02911635769f1193dd
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-07 09:40:06 +0200

    Created topMovies array and Express get route

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  { title: 'The Wizard of Oz', director: 'Victor Fleming' },
+  { title: 'Willy Wonka & the Chocolate Factory', director: 'Mel Stuart' },
+  { title: 'Dawn of the Dead', director: 'George A. Romero' },
+  { title: 'Videodrome', director: 'David Cronenberg' },
+  { title: 'Phantasm', director: 'Don Coscarelli' },
+  { title: 'Phenomena', director: 'Dario Argento' },
+  { title: 'City of the Living Dead', director: 'Lucio Fulci' },
+  { title: 'Hellraiser', director: 'Clive Barker' },
+  { title: 'Prince of Darkness', director: 'John Carpenter' },
+  { title: 'Paprika', director: 'Satoshi Kon' },
+];
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
