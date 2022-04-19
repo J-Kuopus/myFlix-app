38779b7bec106313bed7a36f489e02f19ed3f5ef
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-19 13:03:55 +0200

    Created schema for "users" collection

--- js/models.js
+++ js/models.js
@@ -14,3 +14,12 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+
