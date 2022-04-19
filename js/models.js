f234148d3e0dd84197cdad68d72c7fda1705fe6b
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-19 12:59:35 +0200

    Created schema for "movies" collection

--- js/models.js
+++ js/models.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String,
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    ImagePath: String,
+    Featured: Boolean
+});
+
