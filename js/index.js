8101f69f12831b1ca88ff0dc964f546b1b6b8d3d
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-21 15:17:33 +0200

    Created GET method for finding movie directors

--- js/index.js
+++ js/index.js
@@ -81,6 +81,20 @@ app.get('/genre/:Name', (req, res) => {
 });
 
 // READS and returns info about ONE director by name in JSON format
+app.get('/director/:Name', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Name })
+  .then((movie) => {
+    if (movie) {
+  res.status(200).json(movie.Director.Bio);
+  } else {
+    res.status(400).send('Director not found.');
+  };
+})
+  .catch((err) => {
+  console.error(err);
+  res.status(500).send('Error; ' + err);
+  });
+});
 
 
 // CREATE, allows new users to register
