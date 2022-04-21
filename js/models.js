a7ff91f213df5ef60a48b4242c52d4545ae1bd3c
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-21 15:13:25 +0200

    Added spacing

--- js/models.js
+++ js/models.js
@@ -23,10 +23,12 @@ let userSchema = mongoose.Schema({
 });
 
 let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User',userSchema);
+let User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+
+
 
 
 
