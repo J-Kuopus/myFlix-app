b1760b15abe01aec7126dc5ec112748371db7792
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-21 15:14:58 +0200

    Removed Genres and Directors variables

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ const Models = require('./models.js');
 // Imports Mongoose Models
 const Movies = Models.Movie;
 const Users = Models.User;
-const Genres = Models.Genre;
-const Directors = Models.Director;
 
 // Connects Mongooose to database for CRUD operations
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
