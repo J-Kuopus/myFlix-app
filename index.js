71d56af3a3f73a08c5404c677a524361121c575f
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-26 10:49:19 +0200

    Added validation function/hashed password function

--- index.js
+++ index.js
@@ -198,14 +198,30 @@ app.post('/users',
 // UPDATE, allows users to update username
 app.put(
   '/users/:Username',
+  [
+    check('Username', 'Username is required.').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required.').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid.').isEmail()
+  ],
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+
+    // Check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
