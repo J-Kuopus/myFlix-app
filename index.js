ebaf19ef38c2f20b6473621c05d40d81b3c26c44
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-05 10:41:10 +0200

    Changed for testing

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listening function
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
-  console.log('Listening on Port ' + port);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0',() => {
+  console.log('Listening on Port ' + PORT);
 });
