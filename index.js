76f6ca8eb2d346eb5f29ab3753537b31c3a273dc
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-05 10:43:56 +0200

    Changed back to lowercase name

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listening function
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0',() => {
-  console.log('Listening on Port ' + PORT);
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+  console.log('Listening on Port ' + port);
 });
