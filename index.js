85492353cf3fbe38247767aae928ad9a29aab965
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-19 08:20:40 +0200

    Implemented new cors method

--- index.js
+++ index.js
@@ -41,6 +41,18 @@ app.use(bodyParser.json());
 // Imports CORS module for allowing cross-origin domain access
 const cors = require('cors');
 //app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Imports "auth.js" file. (app) ensures that Express is available in "auth.js".
 let auth = require('./auth')(app);
