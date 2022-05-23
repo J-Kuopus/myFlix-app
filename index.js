298eee9ccf3413286126f57d618d93dbedbc5723
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-23 09:41:20 +0200

    Added GET method for viewing profile info

--- index.js
+++ index.js
@@ -242,6 +242,20 @@ app.put(
   }
 );
 
+// READ--allows users to view thier profile info
+app.get('/users/:Username',  
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    Users.findOne({ username: req.params.username })
+      .then ((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+});
+
 // UPDATE, Allows users to add One Movie to their Favorites list
 app.put(
   '/users/:Username/movies/:MovieID',
